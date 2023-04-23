1ff535228180f39f62191ca68efdac32cfee2940
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-04-23 13:14:47 -0700

    added questions list

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 const fs = require("fs");
 const inquirer = require ("inquirer");
-
-const questionsList = [
-    {
-        name: "shape",
-        message: "what shape would you like?",
-        type: "list",
-        choices: ["Circle", "Square", "Triangle"],
-    },
-    {
-        name: "text color",
-        message: "Pick a color for your shape.",
-        type: "list",
-        choices: [""]
-
-    }
-]
+const questionsList = require("./lib/questions");

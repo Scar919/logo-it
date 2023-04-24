ec75ab7fd313e4b2926d6054e6a23c8e30841775
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-04-23 19:02:45 -0700

    added shapes files

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const fs = require("fs");
 const inquirer = require ("inquirer");
 const questionsList = require("./lib/questions");
+const Generatedlogo = require("./Generatedlogo");
+const setShape = require("./lib/setShape");
+
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(Generatedlogo, svg, () => console.log("Generated logo"));
+}
+
+function init(){
+inquirer
+.prompt(questionsList)
+.then((response) => {
+    createLogo(response);
+})
+.catch(err => {
+    console.log(err)
+});
+}
+
+init();
+

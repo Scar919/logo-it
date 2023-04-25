35e704dfca0a3f036582811704d575f52488369c
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-04-24 19:16:31 -0700

    condensed index.js

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-const fs = require("fs");
 const inquirer = require ("inquirer");
-const questionsList = require("./lib/questions");
-const Generatedlogo = require("./Generatedlogo");
-const setShape = require("./lib/setShape");
+const fs = require("fs");
+//const questions = require("./lib/questions.js");
+const generatedLogo = "./Generatedlogo/logo.svg";
+const setShape = require("./lib/setShape.js");
+
+
+const questions = [
+    {
+        name: "shape",
+        message: "what shape would you like?",
+        type: "list",
+        choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+        name: "shape color",
+        message: "Pick a color for your shape.",
+        type: "list",
+        choices: ["Blue", "Red", "Yellow", "Green"],
+
+    },
+    {
+        name: "text",
+        message: "Please enter a text. (3 characters max)",
+        type: "input",
+    },
+    {
+        name: "text color",
+        message: "Chose a color for your text",
+        type: "list",
+        choices: ["Blue", "Red", "Yellow", "Green"],
+    },
+];
+
 
 function createLogo(response) {
     const svg = setShape(response);
-    fs.writeFile(Generatedlogo, svg, () => console.log("Generated logo"));
+    fs.writeFile(generatedLogo, svg, ()=> console.log("Generated logo.svg"));
 }
 
-function init(){
+function init() {
 inquirer
-.prompt(questionsList)
+.prompt(questions)
 .then((response) => {
     createLogo(response);
 })

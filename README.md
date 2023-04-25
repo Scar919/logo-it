922cc87aba9ae6c7b0706ddcd6c5af61eae97570
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-04-24 21:38:51 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # logo-it
+
+# Description
+The motivation behind this was to build a logo generator so that developers could create logos without needing to spend money for a graphic designer. It is an easy process which can be done within the command line to quickly genrate a logo. There is a limit between three shapes (square, triangle, and circle) and three characters max.
+
+[walk-through](https://drive.google.com/file/d/15kyVhqojgOSG-RJWMmPBFkYbd7CXNGQI/view)
+
+![IMG](./img/Screenshot%202023-04-24%20at%209.27.29%20PM.png)
+
+# Installation
+In order to install this feature you should do the following:
+
+Download Node onto your computer
+Fork the repo from github
+Clone the repo from github
+Run "npm install"
+Run "npm install jest"
+
+# Usage
+
+Once you have followed the above steps run "node index.js" within the command line termainal. You will see the questions prompted within the terminal, make sure to fill out every question. Once all questions have been answered it will populate a generated logo
+
+# Tests
+
+Each class is tested usind render() menthod which return a string to that correct file. to run these tests enter "npm test" in command line and jest will run them for you.
+
+# License 
+N/A
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+// const given names to be called within function
 const inquirer = require ("inquirer");
 const fs = require("fs");
-//const questions = require("./lib/questions.js");
 const generatedLogo = "./Generatedlogo/logo.svg";
 const setShape = require("./lib/setShape.js");
+const colorKeywords = ['aliceblue', 'antiquewhite', 'aqua', 'aquaMarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgrey', 'darkgreen', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'grey', 'green', 'greenyellow', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgrey', 'lightgreen', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
 
-
+//array of questions to populate in command line terminal
 const questions = [
     {
         name: "shape",
@@ -13,31 +14,108 @@ const questions = [
         choices: ["Circle", "Square", "Triangle"],
     },
     {
-        name: "shape color",
-        message: "Pick a color for your shape.",
+        name: "colorShapeChoice",
+        message: "Pick a color for your shape. please choose:",
         type: "list",
-        choices: ["Blue", "Red", "Yellow", "Green"],
+        choices: ["color keyword", "hexdecimal"],
 
     },
+    //input for shape color with text or hexadecimal
+    {   type: "input",
+        name: "shapeColor",
+        message: "Please enter a color",
+        when: (answers) => {
+            if(answers.colorShapeChoice === "color keyword"){
+                return true;
+            }
+            return false;
+        },
+        validate: (answer) => {
+            let answerLowercase = answer.toLowerCase();
+            for (var i = 0, len = colorKeywords.length; i < len; ++i) {
+                if (answerLowercase.indexOf(colorKeywords[i]) != -1){
+                    return true;
+                }}
+                return console.log("Please enter valid choice")
+            }
+        },
+    {
+        type: "input",
+        name: "shapeColor",
+        messgae: "Enter a hexdaecimal number (#ffff)",
+        when: (answers) => {
+            if(answers.colorShapeChoice === "hexadecimal"){
+                return true;
+            }
+            return false;
+        },
+        validate: (answer) => {
+            const hexRegEx = '^#[A-Fa-f0-9]{6}$'
+            if (!answer.match(hexRegEx)) {
+                return console.log("Enter valid hexadecimal")
+            }
+            return true;
+        }
+    },
+
     {
         name: "text",
         message: "Please enter a text. (3 characters max)",
         type: "input",
     },
     {
-        name: "text color",
+        name: "textColor",
         message: "Chose a color for your text",
         type: "list",
-        choices: ["Blue", "Red", "Yellow", "Green"],
+        choices: ["color keyword", "hexadecimal"],
     },
+    //input for text color using key word or hexadecimal 
+    {
+        type: "input",
+        name: "textColor",
+        message: "Please enter a color",
+        when: (answers) => {
+            if(answers.textColorChoice === "color keyword"){
+                return true;
+            }
+            return false;
+        },
+        validate: (answer) => {
+            let answerLowercase = answer.toLowerCase();
+            for (var i = 0, len = colorKeywords.length; i < len; ++i) {
+                if (answerLowercase.indexOf(colorKeywords[i]) != -1){
+                    return true;
+                }}
+                return console.log("Please enter valid choice")
+            }
+    },
+    {
+        type: "input",
+        name: "textColor",
+        messgae: "Enter a hexdaecimal number (#ffff)",
+        when: (answers) => {
+            if(answers.textColorChoice === "hexadecimal"){
+                return true;
+            }
+            return false;
+        },
+        validate: (answer) => {
+            const hexRegEx = '^#[A-Fa-f0-9]{6}$'
+            if (!answer.match(hexRegEx)) {
+                return console.log("Enter valid hexadecimal")
+            }
+            return true;
+        }
+    },
+    
 ];
 
-
+//function to create svg logo file 
 function createLogo(response) {
     const svg = setShape(response);
     fs.writeFile(generatedLogo, svg, ()=> console.log("Generated logo.svg"));
 }
-
+//initializing questions to create logo & catch errors
 function init() {
 inquirer
 .prompt(questions)
